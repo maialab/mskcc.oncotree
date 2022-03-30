ea756d9888e0b39d11ec27c2e1313b4a9df6787f
Ramiro Magno <ramiro.magno@gmail.com>
2022-03-30 14:38:14 +0100

    Add doc to `nci_to_oncotree()` and `oncotree_to_nci()`

--- R/oncotree_to_nci.R
+++ R/oncotree_to_nci.R
@@ -1,3 +1,29 @@
+#' Map OncoTree to NCIt codes
+#'
+#' This function maps OncoTree codes to  National Cancer Institute Thesaurus (NCIt) codes.
+#'
+#' @param oncotree_code OncoTree codes.
+#' @param oncotree_version OncoTree database release version.
+#' @param expand Whether to expand one-to-many mappings. If `TRUE`, one-to-many mappings are expanded into several rows in the output.
+#' @param keep_empty OncoTree codes that do not map to NCI have the `nci_code` with `NA` if `keep_empty = TRUE`. Use `keep_empty = FALSE`, to remove the mapping (row) altogether from the output.
+#'
+#' @return A [tibble][tibble::tibble-package] of two variables: `oncotree_code` and `nci_code`.
+#' @md
+#' @examples
+#' # Leave `oncotree_code` empty to return mappings for all OncoTree codes
+#' oncotree_to_nci()
+#'
+#' # Map a few selected OncoTree codes
+#' oncotree_to_nci(oncotree_code = c('PAOS', 'SCST', 'ITLPDGI', 'SRCCR'))
+#'
+#' # Use `expand` to make sure the column `nci_code` is a character vector and
+#' # not a list-column. One-to-many mappings will result in more than row with
+#' # `oncotree_code` values repeated.
+#' oncotree_to_nci(oncotree_code = c('PAOS', 'SCST', 'ITLPDGI', 'SRCCR'), expand = TRUE)
+#'
+#' # Use `keep_empty` to drop or keep one-to-none mappings
+#' oncotree_to_nci(oncotree_code = c('PAOS', 'SCST', 'ITLPDGI', 'SRCCR'), expand = TRUE, keep_empty = FALSE)
+#'
 #' @importFrom rlang .data
 #' @export
 oncotree_to_nci <-
@@ -24,6 +50,29 @@ oncotree_to_nci <-
     return(mapping)
   }
 
+#' Map NCI to OncoTree codes
+#'
+#' This function maps National Cancer Institute Thesaurus (NCIt) codes to OncoTree codes.
+#'
+#' @param nci_code NCI codes.
+#' @param oncotree_version OncoTree database release version.
+#' @param expand Whether to expand one-to-many mappings. If `TRUE`, one-to-many mappings are expanded into several rows in the output.
+#'
+#' @return A [tibble][tibble::tibble-package] of two variables: `nci_code` and `oncotree_code`.
+#' @md
+#'
+#' @examples
+#' # Leave `nci_code` empty to return mappings for all NCI codes
+#' nci_to_oncotree()
+#'
+#' # Map a few selected OncoTree codes
+#' nci_to_oncotree(nci_code = c('C8969', 'C4862', 'C9168', 'C7967'))
+#'
+#' # Use `expand` to make sure the column `oncotree_code` is a character vector and
+#' # not a list-column. One-to-many mappings will result in more than row with
+#' # `oncotree_code` values repeated.
+#' nci_to_oncotree(nci_code = c('C8969', 'C4862', 'C9168', 'C7967'), expand = TRUE)
+#'
 #' @importFrom rlang .data
 #' @export
 nci_to_oncotree <-
