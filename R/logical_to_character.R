a42d1167b48bbbadd095a19341c57399d0636181
Ramiro Magno <ramiro.magno@gmail.com>
2022-04-01 00:39:16 +0100

    Add `logical_to_character()`

--- R/logical_to_character.R
+++ R/logical_to_character.R
@@ -1,3 +1,14 @@
+#' Converts a logical vector to character
+#'
+#' This function converts a logical vector to a character vector allowing to
+#' control the case and even to convert to integer 0 or 1 if desired.
+#'
+#' @param x A logical vector.
+#' @param format Controls how the text is generated.
+#'
+#' @return A character vector.
+#'
+#' @keywords internal
 logical_to_character <- function(x, format = c('lowercase', 'uppercase', 'titlecase', 'integer')) {
 
   if(!is.logical(x)) stop('`x` must be a logical vector')
